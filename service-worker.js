b4840c4664ff90d1224f597b262df6ff3e0beaec
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-09-25 16:50:25 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-app/precache-manifest.7dc72155084cefc61f411a33221ada10.js"
+  "/portfolio-app/precache-manifest.7d134c49deb6db2c25cad0ff787290a5.js"
 );
 
 self.addEventListener('message', (event) => {
